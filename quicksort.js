c739cb0e2e8f9fb4132e2b299136d34ffc8ff3ca
doug <drcox23@gmail.com>
2018-07-26 21:34:12 -1000

    quicksort

---
+++ quicksort.js
@@ -0,0 +1,25 @@
+let array = [10, 3, 2, -1, 34, 4, 53, 100, 563, 45, 3];
+
+// console.time("run");
+quickSort = array => {
+  if (array.length < 2) {
+    return array;
+  }
+
+  let pivot = array[0];
+  let left = [];
+  let right = [];
+
+  for (let i = 1; i < array.length; i++) {
+    if (array[i] < pivot) {
+      left.push(array[i]);
+    } else {
+      right.push(array[i]);
+    }
+  }
+
+  return quickSort(left).concat(pivot, quickSort(right));
+};
+
+console.log(quickSort(array));
+// console.timeEnd("run");
